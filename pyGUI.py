d1958e6e2e0b94fcfb8a2283db96b91db8db53ef
P-Angelica <67887104+P-Angelica@users.noreply.github.com>
2023-04-23 23:00:36 -0400

    Update pyGUI.py
    This code shows the start of a tab layout for GUI

--- pyGUI.py
+++ pyGUI.py
@@ -1,75 +1,49 @@
-### Python GUI testing
-
 import sys
-from random import choice
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPalette, QColor
+from PySide6.QtWidgets import (
+    QVBoxLayout,
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QTabWidget,
+    QWidget,
+)
 
-from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QMenu, QPushButton
 
-# window_titles = [
-#     'My App',
-#     'My App',
-#     'Still My App',
-#     'Still My App',
-#     'What on earth',
-#     'What on earth',
-#     'This is surprising',
-#     'This is surprising',
-#     'Something went wrong'
-# ]
+class Color(QWidget):
 
-# Subclass QMainWindow to customize your application's main window
-class CustomButton(QPushButton):
-    def event(self, e):
-        e.ignore()
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
-        self.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.on_context_menu)
+        self.setWindowTitle("Lenitcular Image Creator")
 
-    def mousePressEvent(self, event):
-        print("Mouse pressed!")
-        super().mousePressEvent(event)
 
-    def on_context_menu(self, pos):
-        context = QMenu(self)
-        context.addAction(QAction("test 1", self))
-        context.addAction(QAction("test 2", self))
-        context.addAction(QAction("test 3", self))
-        context.exec_(self.mapToGlobal(pos))
+        ## Create Top Level Layout
+        layout = QVBoxLayout()
+        self.setLayout(layout)
 
-    def contextMenuEvent(self, e):
-        context = QMenu(self)
-        context.addAction(QAction("test 1", self))
-        context.addAction(QAction("test 2", self))
-        context.addAction(QAction("test 3", self))
-        context.exec_(e.globalPos())
+        ## Create tab widget n tabs
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.North)
+        tabs.setMovable(True)
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print("Setting title:  %s" % new_window_title)
-        self.setWindowTitle(new_window_title)
+        for n, color in enumerate(["red", "green", "blue", "yellow"]):
+            tabs.addTab(Color(color), color)
 
-    def the_window_title_changed(self, window_title):
-        print("Window title changed: %s" % window_title)
-
-        if window_title == 'Something went wrong':
-            self.button.setDisabled(True)
-
-    def the_button_was_toggled(self, checked):
-        self.button_is_checked = checked
-
-        print(self.button_is_checked)
-
-    def the_button_was_released(self):
-        self.button_is_checked = self.button.isChecked()
-
-        print(self.button_is_checked)
+        layout.addWidget(tabs)
+        self.setCentralWidget(tabs)
 
 
 app = QApplication(sys.argv)
@@ -78,57 +52,3 @@ window = MainWindow()
 window.show()
 
 app.exec_()
-
-
-# # https://www.tutorialspoint.com/how-to-place-an-image-into-a-frame-in-tkinter // GeeksForGeeks
-# # Import required libraries
-# from tkinter import *
-# from PIL import ImageTk, Image
-#
-# # Create an instance of tkinter window
-# win = Tk()
-#
-# # Define the geometry of the window
-# win.title("Lenticular Image Creator")
-# win.geometry("700x500")
-#
-# frame = Frame(win, width=600, height=400)
-# frame.pack()
-# frame.place(anchor='center', relx=0.5, rely=0.5)
-#
-# menu = Menu(win)
-# item = Menu(menu)
-# item.add_command(label='New')
-# menu.add_cascade(label='File', menu=item)
-# win.config(menu=menu)
-#
-# # adding a label to the root window
-# lbl = Label(win, text="Ready to make an image?")
-# lbl.grid()
-#
-# # adding Entry Field
-# txt = Entry(win, width=10)
-# txt.grid(column=1, row=0)
-#
-#
-# # function to display user text when
-# # button is clicked
-# def clicked():
-#     res = "You wrote" + txt.get()
-#     lbl.configure(text=res)
-#
-#
-# # button widget with red color text inside
-# btn = Button(win, text="Click me",
-#              fg="red", command=clicked)
-# # Set Button Grid
-# btn.grid(column=2, row=0)
-#
-# # Create an object of tkinter ImageTk
-# img = ImageTk.PhotoImage(Image.open("images/alt1.png"))
-#
-# # Create a Label Widget to display the text or Image
-# label = Label(frame, image = img)
-# label.pack()
-#
-# win.mainloop()
